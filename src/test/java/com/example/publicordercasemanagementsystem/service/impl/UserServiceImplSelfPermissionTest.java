5caf3205f6e86929a10a1262ddd38c55ceefe093
lhx <a3343557361@163.com>
2026-04-13 22:38:15 +0800

    Changes

--- src/test/java/com/example/publicordercasemanagementsystem/service/impl/UserServiceImplSelfPermissionTest.java
+++ src/test/java/com/example/publicordercasemanagementsystem/service/impl/UserServiceImplSelfPermissionTest.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
